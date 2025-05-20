cf94c05ed2330856886063f03e694e67cc3d6bb0
Borat <your.email@example.com>
2025-05-20 15:39:46 +0200

    DEV add generators for ticker and trade

--- python_pipeline/src/python_pipeline/data_generators/__init__.py
+++ python_pipeline/src/python_pipeline/data_generators/__init__.py
@@ -7,3 +7,5 @@ Contains data generators for creating fake/test data.
 from python_pipeline.data_generators.ack_generator import AckDataGenerator
 from python_pipeline.data_generators.base_generator import BaseDataGenerator
 from python_pipeline.data_generators.factory import DataGeneratorFactory
+from python_pipeline.data_generators.ticker_generator import TickerGenerator
+from python_pipeline.data_generators.trade_generator import TradeGenerator
--- python_pipeline/src/python_pipeline/data_generators/factory.py
+++ python_pipeline/src/python_pipeline/data_generators/factory.py
@@ -6,6 +6,8 @@ from typing import Dict, Type
 
 from python_pipeline.data_generators.ack_generator import AckDataGenerator
 from python_pipeline.data_generators.base_generator import BaseDataGenerator
+from python_pipeline.data_generators.ticker_generator import TickerGenerator
+from python_pipeline.data_generators.trade_generator import TradeGenerator
 
 # Import other generators as they're created
 
@@ -15,6 +17,8 @@ class DataGeneratorFactory:
     # Registry of available generators
     _generators = {
         "ack": AckDataGenerator,
+        "ticker": TickerGenerator,
+        "trade": TradeGenerator,
         # Add more generators as they're implemented
     }
     
